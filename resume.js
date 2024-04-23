ca78577ba65c870084d3ed887345da484db32391
kumaranselvamani <kumaranmech5@gmail.com>
2024-04-23 09:34:48 +0530

    commit

---
+++ resume.js
@@ -0,0 +1,30 @@
+const resume = {
+      name: "kumaran",
+      label: "Developer",
+      email: "kumaranmech5@gmail.com",
+      phone: "+91 7708089394",
+      address: {
+        city: "chennai",
+        country: "india"
+      },
+      education: {
+      institution: "ANNA University",
+      dipartment: "Mechanical Engineering",
+      studyType: "Bachelor of Engineering"
+      },
+      certificates: {
+      name: "webdevelopment bootcamp-2024",
+      date: "2024-Mar",
+      issuer: "Udemy"
+      },
+      skills: ["javascript", "reactjs", "html", "css", "mongoDB", "Nodejs"],
+      languages: ["english", "tamil"],
+      projects: {
+      name: "TO DO LIST APP",
+      technologies:["javascript","css","html"],
+      url: "https://project.com/https://kumaranselvamani.github.io/todolist/"
+      },
+      hobbies: ["reading", "gaming"],
+    }
+
+console.log(resume);
